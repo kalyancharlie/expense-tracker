65d4c0501079df96859d0fb2cef9e06f90633135
kalyancharlie <kalyancharlie@gmail.com>
2022-04-01 10:40:58 +0530

    Nav button active styles fix

--- client/src/components/CustomLink.js
+++ client/src/components/CustomLink.js
@@ -1,14 +1,24 @@
 import { useResolvedPath, useMatch, Link } from "react-router-dom";
 
 const CustomLink = ({ to, children, ...props }) => {
-  const resolvedPath = useResolvedPath(to)
-  const matchStatus = useMatch({ path: resolvedPath.pathname, end: true })
+  const resolvedPath = useResolvedPath(to);
+  const matchStatus = useMatch({ path: resolvedPath.pathname, end: true });
 
-  return <div className={matchStatus ? 'footer-button selected' : 'footer-button'}>
-    <Link to={to} {...props}>
-      {children}
-    </Link>
-  </div>
-}
+  return (
+    <div className="footer-button">
+      <Link
+        className={
+          matchStatus
+            ? "footer-button navbar__link selected"
+            : "footer-button navbar__link"
+        }
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+    </div>
+  );
+};
 
 export default CustomLink;
